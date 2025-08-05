b3d67364681f083eb1d0871a62658952a55eefb7
meylsarachmasafitri518 <meylsarachmasafitri518@gmail.com>
2025-08-05 12:58:13 +0700

    Add files via upload

---
+++ tugas1. cpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   cout << "MEYLSA RACHMA SAFITRI " << endl;
+   cout << "X MIPA" << endl;
+   cout << "MA AL-MANSHURIYAH" << endl;
+   cout << "CIANJUR 24 mei 2010" << endl;
+   cout << "Kp pasirbaru Ds pasirbaru Kec pagelaran Kab cianjur" << endl;
+}
+    
